98c022f3ba0b3704cfd7e63f77df09ae7611005d
Joel Buckley <github@github.joelbuckley.com.au>
2019-12-09 17:57:25 +1100

    Added database wrapper to keep DB connections to 1

--- services/DatabaseService.php
+++ services/DatabaseService.php
@@ -4,8 +4,70 @@ namespace Grocy\Services;
 
 use \Grocy\Services\ApplicationService;
 
+class PDOWrap
+{
+	private $instance = null;
+	public function __construct(){
+		$pars = func_get_args();
+		$this->instance = is_object($obj = 'PDO') ? $obj : new \PDO($pars[0], $pars[1], $pars[2], array(\PDO::ATTR_PERSISTENT => true, \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true));
+		return $this;
+	}
+
+	public function __call($name, $pars) {
+		$result = null;
+
+		$fp = fopen('/usr/local/var/www/grocy/sql.log', 'a');
+		fwrite($fp, "PDO::".$name." called with arguments:- ".implode( ", ", $pars)."\n");
+		$time_start = microtime(true);
+		if(in_array($name, array("exec","query")))
+		{
+			// if($name != 'exec') {
+
+				$pos = strpos(implode( ", ", $pars), 'product_group_id');
+				if($pos !== false) {
+					$wantedKeys = array('function', 'line', 'file', 'class');
+					$i = 0;
+					$debug = debug_backtrace();
+					while(array_key_exists($i, $debug)) {
+						$a = array_intersect_key(debug_backtrace()[$i], array_flip($wantedKeys));
+						print_r($a);
+						$i++;
+					}
+					die();
+				}
+
+				// fwrite($fp, array_values($pars)[0] . "\n");
+				$result = call_user_func_array([$this->instance,$name],$pars);
+			// }
+		} else {
+			$result = call_user_func_array([$this->instance,$name],$pars);
+		}
+		#fwrite($fp, "Total execution time in seconds: " . round((microtime(true) - $time_start),6) . "\n");
+		#fclose($fp);
+		return $result;
+	}
+}
+
 class DatabaseService
 {
+	private static $instance = null;
+
+	public static function getInstance()
+	{
+		if (self::$instance == null)
+		{
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	#private static function __construct()
+	#{
+	#	parent::__construct();
+	#}
+
+
 	private function GetDbFilePath()
 	{
 		if (GROCY_MODE === 'demo' || GROCY_MODE === 'prerelease')
@@ -16,34 +78,54 @@ class DatabaseService
 		return GROCY_DATAPATH . '/grocy.db';
 	}
 
-	private $DbConnectionRaw;
+	#private $DbConnectionRaw;
+    private static $DbConnectionRaw = null;
 	/**
 	 * @return \PDO
 	 */
 	public function GetDbConnectionRaw()
 	{
-		if ($this->DbConnectionRaw == null)
+		if (self::$DbConnectionRaw == null)
+		#if ($this->DbConnectionRaw == null)
 		{
-			$pdo = new \PDO('sqlite:' . $this->GetDbFilePath());
+			#$fp = fopen('/www/data/sql.log', 'a');
+			#fwrite($fp, "+++Creating new PDO object\n");
+			#$time_start = microtime(true);
+			$pdo = new PDOWrap("mysql:host=127.0.0.1;dbname=grocy", 'grocy', 'grocy');
 			$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-			$this->DbConnectionRaw = $pdo;
+			self::$DbConnectionRaw = $pdo;
+			#$this->DbConnectionRaw = $pdo;
+			#fwrite($fp, "+++Total execution time in seconds: " . round((microtime(true) - $time_start),6) . "\n");
+			#fwrite($fp, "+++object created\n");
+			#fclose($fp);
 		}
 
-		return $this->DbConnectionRaw;
+		return self::$DbConnectionRaw;
+		#return $this->DbConnectionRaw;
 	}
 
-	private $DbConnection;
+	#private $DbConnection;
+	private static $DbConnection = null;
 	/**
 	 * @return \LessQL\Database
 	 */
 	public function GetDbConnection()
 	{
-		if ($this->DbConnection == null)
+		if (self::$DbConnection == null)
+		#if ($this->DbConnection == null)
 		{
-			$this->DbConnection = new \LessQL\Database($this->GetDbConnectionRaw());
+			#$fp = fopen('/www/data/sql.log', 'a');
+			#fwrite($fp, "---creating new LessQL::Database object\n");
+			#$time_start = microtime(true);
+			self::$DbConnection = new \LessQL\Database($this->GetDbConnectionRaw());
+			#$this->DbConnection = new \LessQL\Database($this->GetDbConnectionRaw());
+			#fwrite($fp, "---Total execution time in seconds: " . round((microtime(true) - $time_start),6) . "\n");
+			#fwrite($fp, "---object created\n");
+			#fclose($fp);
 		}
 
-		return $this->DbConnection;
+		return self::$DbConnection;
+		#return $this->DbConnection;
 	}
 
 	/**
@@ -66,12 +148,7 @@ class DatabaseService
 	public function ExecuteDbQuery(string $sql)
 	{
 		$pdo = $this->GetDbConnectionRaw();
-		if ($this->ExecuteDbStatement($sql) === true)
-		{
-			return $pdo->query($sql);
-		}
-
-		return false;
+		return $pdo->query($sql);
 	}
 
 	public function GetDbChangedTime()
@@ -81,6 +158,6 @@ class DatabaseService
 
 	public function SetDbChangedTime($dateTime)
 	{
-		touch($this->GetDbFilePath(), strtotime($dateTime));
+		// touch($this->GetDbFilePath(), strtotime($dateTime));
 	}
 }
